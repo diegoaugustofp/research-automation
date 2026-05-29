6c8ce148c9bde730b54302b1f350ed3c83630b2c
Diego Augusto <diegoaugustofp@gmail.com>
2026-05-28 23:46:09 -0300

    chore: initialize spring boot project with base dependencies

---
+++ src/main/java/br/com/diegoaugustofp/research_automation/ResearchAutomationApplication.java
@@ -0,0 +1,13 @@
+package br.com.diegoaugustofp.research_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diegoaugustofp/research_automation/ResearchAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diegoaugustofp.research_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
